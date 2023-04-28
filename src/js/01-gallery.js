fad04b9a9b6797242416c68af39913d2a4d11d1f
kutsarvasya <vkutsar89@gmail.com>
2023-04-28 16:15:17 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGallery = (pictures) => {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`;
+    })
+    .join("");
+};
+
+galleryEl.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+// console.log(galleryItems);
