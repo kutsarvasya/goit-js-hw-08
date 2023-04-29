3bef7591d6a0713f702d2c0cdbf0a83d5b9aefec
kutsarvasya <vkutsar89@gmail.com>
2023-04-29 10:29:10 +0200

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+ const iframe = document.querySelector('iframe');
+ const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+
+ function callback(data) {
+     localStorage.setItem('videoplayer-current-time', data.seconds);
+            console.log(data.seconds);
+ }
+ 
+ player.on('timeupdate', throttle(callback, 1000))
+ 
+
+
+
 
